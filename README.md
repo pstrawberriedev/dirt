eb85728e4e46ada4b98e8000ce14f22b5f8cdd28
pstrawberriedev <pstrawberriedev@gmail.com>
2017-04-10 14:48:12 -0600

    initial up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dirt
-super wank front-end prototyping with gulp and sass
+# Dirt!
+A simple-ish boilerplate for prototyping a standard HTML5 Site
+
+### Included
+- Starter code (html boilerplate, sass mixins)
+- Gulp: SASS -> CSS
+- Gulp: ES6 -> ES5
+
+### Flow
+``git clone https://github.com/pstrawberriedev/dirt.git``  
+``cd dirt && npm install``  
+``npm start`` -or- ``gulp``
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+//* Dependencies
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
+var sass = require('gulp-sass')
+var cleanCSS = require('gulp-clean-css')
+
+//* Options
+var sassMaster = './styles/app.scss'
+var sassFiles = './styles/**/*.scss'
+var cssDest = './styles/dist'
+
+//* Task: Compile and minify SASS
+gulp.task('sass', function () {
+  console.log('--------- Sassin Up! ---------')
+  return gulp.src(sassMaster)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie9'}))
+    .pipe(concat('app.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(cssDest))
+})
+
+//* Task: Watch .scss and .js
+gulp.task('watch', function() {
+    gulp.watch(sassFiles,['sass'])
+})
+
+//* Task: Default
+gulp.task('default', ['sass', 'watch'])
